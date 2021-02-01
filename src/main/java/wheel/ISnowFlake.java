16e8a2e1b13d0fba128aa195f5eb50f12a331c65
aster42 <mxl73@outlook.com>
2021-02-01 10:40:14 +0800

    snow flake

---
+++ src/main/java/wheel/ISnowFlake.java
@@ -0,0 +1,82 @@
+package wheel;
+
+/**
+ * generate unique id (64 bit)
+ * 1 bit for placeholder
+ * 41 bit for timestamp
+ * 10 machine id
+ * 12 serialize id (4096 keys per second)
+ *
+ * https://tech.meituan.com/2017/04/21/mt-leaf.html
+ * */
+public class ISnowFlake {
+
+    private final static long START_STAMP = 1480166465631L;
+    private final static long SEQUENCE_BIT = 12;
+    private final static long MACHINE_BIT = 5;
+    private final static long DATA_CENTER_BIT = 5;
+
+    private final static long MAX_DATA_CENTER_NUM = ~(-1L << DATA_CENTER_BIT);
+    private final static long MAX_MACHINE_NUM = ~(-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = ~(-1L << SEQUENCE_BIT);
+
+    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+    private final static long DATA_CENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long TIMESTAMP_LEFT = DATA_CENTER_LEFT + DATA_CENTER_BIT;
+
+    private long dataCenterId;
+    private long machineId;
+    private long sequence = 0L;
+    private long lastStamp = -1L;
+
+    public ISnowFlake(long dataCenterId, long machineId) {
+        if (dataCenterId > MAX_DATA_CENTER_NUM || dataCenterId < 0) {
+            throw new IllegalArgumentException("dataCenterId can't be greater than MAX_DATA_CENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.dataCenterId = dataCenterId;
+        this.machineId = machineId;
+    }
+
+    public synchronized long nextId() {
+        long currStamp = getNewStamp();
+        if (currStamp < lastStamp) {
+            throw new RuntimeException("Clock moved backwards.  Refusing to generate id");
+        }
+
+        if (currStamp == lastStamp) {
+            sequence = (sequence + 1) & MAX_SEQUENCE;
+            if (sequence == 0L) {
+                currStamp = getNextMill();
+            }
+        } else {
+            sequence = 0L;
+        }
+
+        lastStamp = currStamp;
+
+        return (currStamp - START_STAMP) << TIMESTAMP_LEFT
+                | dataCenterId << DATA_CENTER_LEFT
+                | machineId << MACHINE_LEFT
+                | sequence;
+    }
+
+    private long getNextMill() {
+        long mill = getNewStamp();
+        while (mill <= lastStamp) {
+            mill = getNewStamp();
+        }
+        return mill;
+    }
+
+    private long getNewStamp() {
+        return System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) {
+        ISnowFlake snowFlake = new ISnowFlake(3L, 10L);
+        System.out.println("snowFlake.nextId() = " + snowFlake.nextId());
+    }
+}
